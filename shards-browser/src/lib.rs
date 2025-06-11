10b4bef31413285a424288650e53c28596342f17
Daniil Shigapov <me@matht.in>
2025-06-11 21:42:25 +0200

    Add dependencies for logging and graphics stack

--- shards-browser/src/lib.rs
+++ shards-browser/src/lib.rs
@@ -1,14 +1,36 @@
 mod utils;
 
+#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
+use std::sync::OnceLock;
+
+static STARTED: OnceLock<()> = OnceLock::new();
+
+#[cfg(target_arch = "wasm32")]
+fn init_log() {
+    use log::Level;
+    console_log::init_with_level(Level::Trace).expect("error initializing log");
 }
+
+#[cfg(not(target_arch = "wasm32"))]
+fn init_log() {
+    env_logger::init();
+}  
+
+fn first_startup() {
+    init_log();
+    log::debug!("Shards browser starting!");
+
+    #[cfg(target_arch = "wasm32")]
+    utils::set_panic_hook();
+
+    log::debug!("Shards browser started!");
+}
+
 
 #[wasm_bindgen]
 pub fn start() {
-    log("Hello, shards browser!");
+    STARTED.get_or_init(first_startup);
+    log::debug!("Start click");
 }
