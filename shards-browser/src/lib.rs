197eaa69b094182cd2932aedf50ad5043135f709
Daniil Shigapov <me@matht.in>
2025-06-19 03:54:45 +0200

    Add unit test for app start

--- shards-browser/src/lib.rs
+++ shards-browser/src/lib.rs
@@ -28,3 +28,67 @@ pub fn start() {
 pub fn start() {
     start_impl()
 }
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use web_time::{Instant, Duration};
+
+    const MAX_START_WARN_SECS: u64 = 4;
+    const MAX_START_SECS: u64 = 16;
+    const MAX_SECOND_START_MS: u64 = 256;
+
+    #[test]
+    fn it_starts_quick_enough() {
+        timed_start_one_pass();
+
+        const MAX_SECOND_START_DURATION: Duration = Duration::from_millis(MAX_SECOND_START_MS);
+
+        let second_pass_duration = timed_start_one_pass();
+
+        assert!(second_pass_duration < MAX_SECOND_START_DURATION, "Slow second start (should be effectively empty)!");
+    }
+
+    fn timed_start_one_pass() -> Duration {
+        let h  = std::thread::spawn(start);
+
+        const CHECK_SLEEP_DURATION: Duration = Duration::from_millis(MAX_SECOND_START_MS / 2);
+        const MAX_DURATION_ALERT: Duration = Duration::from_secs(MAX_START_WARN_SECS);
+        const MAX_DURATION: Duration = Duration::from_secs(MAX_START_SECS);
+
+        assert!(MAX_DURATION_ALERT < MAX_DURATION);
+
+        let mut real_time_measure = Instant::now();
+        let mut passed: Duration = Default::default();
+
+        while passed < MAX_DURATION_ALERT {
+            if h.is_finished() {
+                break;
+            }
+            std::thread::sleep(CHECK_SLEEP_DURATION);
+            let new_real_time_measure = Instant::now();
+            passed += new_real_time_measure - real_time_measure;
+            real_time_measure = new_real_time_measure;
+        }
+
+        if passed >= MAX_DURATION_ALERT {
+            log::warn!("[timed_start_one_pass] Slow start!");
+        }
+
+        while passed < MAX_DURATION {
+            if h.is_finished() {
+                break;
+            }
+            std::thread::sleep(CHECK_SLEEP_DURATION);
+            let new_real_time_measure = Instant::now();
+            passed += new_real_time_measure - real_time_measure;
+            real_time_measure = new_real_time_measure;
+        }
+
+        assert!(passed < MAX_DURATION, "Starting too long!");
+
+        return passed;
+    }
+}
